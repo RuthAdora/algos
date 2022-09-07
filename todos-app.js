b9a5b9935c114108cd04aad98ffb1c5d629dbbdf
RuthAdora <adoraruth01@gmail.com>
2022-09-07 12:51:03 +0300

    first commit

---
+++ todos-app.js
@@ -0,0 +1,6 @@
+console.log("aswesome todos")
+
+// add new element
+const newparagraph = document.createElement("p");
+newparagraph.textContent = "irene likes dancing";
+document.querySelector("body").appendChild(newparagraph);
